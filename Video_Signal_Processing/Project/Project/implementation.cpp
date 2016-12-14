1a34868247a4b6cd20f277e0f773057acde7ae3a
yvon-shong <yvon-shong@foxmail.com>
2016-12-14 14:02:17 +0900

    build vs project in opencv

---
+++ Video_Signal_Processing/Project/Project/implementation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+
+using namespace cv;
+
+int main() {
+	// 读入一张图片（游戏原画）    
+	Mat img = imread("D:\\OneDrive\\图片\\MyerSplash\\pic.jpg");
+	// 创建一个名为 "游戏原画"窗口    
+	namedWindow("游戏原画");
+	// 在窗口中显示游戏原画    
+	imshow("游戏原画", img);
+	// 等待6000 ms后窗口自动关闭    
+	waitKey(12000);
+}
